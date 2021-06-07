db7f2ecf5cd104c07f96e9e9daf38fd8614d1548
jaimerodliz <seprom19@gmail.com>
2021-06-06 18:24:50 -0600

    Mensaje solucionado

--- index.js
+++ index.js
@@ -95,6 +95,6 @@ app.use('/', routes());
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3001;
 app.listen(port,host, () =>{
-    console.log('El servidor esta funcionando')
+    console.log('El servidor esta LISTO!')
 });
 
